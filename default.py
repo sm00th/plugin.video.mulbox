fc1373abb10162c27e8e045fe3468c9b3ab83317
Artem Savkov <artem.savkov@gmail.com>
2014-04-03 23:00:28 +0400

    Dialogboxes on errors
    Displaying dialog boxes on playlist errors

--- default.py
+++ default.py
@@ -1,16 +1,16 @@
-import os
+import os, sys
 import urllib2
 import xbmcaddon, xbmcgui, xbmcplugin, xbmc
 
 pluginUrl = sys.argv[0]
 pluginHandle = int(sys.argv[1])
 pluginQuery = sys.argv[2]
-__settings__ = xbmcaddon.Addon(id='plugin.video.mulbox')
-__language__ = __settings__.getLocalizedString
+addon = xbmcaddon.Addon(id='plugin.video.mulbox')
+__language__ = addon.getLocalizedString
 
 try:
-  uhash = __settings__.getSetting('uhash')
-  proto = int(__settings__.getSetting('proto')) + 1
+  uhash = addon.getSetting('uhash')
+  proto = int(addon.getSetting('proto')) + 1
 except(ValueError):
   proto = 1
 
@@ -24,14 +24,24 @@ XBMC_PLAYLIST = xbmc.translatePath("special://temp/mulbox.m3u")
 print "playlist url: %s" % PLAYLIST_URL
 print "ondisk path: %s" % XBMC_PLAYLIST
 
-pldata = urllib2.urlopen(PLAYLIST_URL)
-pfile = open(XBMC_PLAYLIST, 'w')
-pfile.write(pldata.read())
-pldata.close()
-pfile.close()
+try:
+  pldata = urllib2.urlopen(PLAYLIST_URL)
+  pfile = open(XBMC_PLAYLIST, 'w')
+  pfile.write(pldata.read())
+  pldata.close()
+  pfile.close()
+except:
+  dialog = xbmcgui.Dialog()
+  dialog.ok(addon.getLocalizedString(30200), addon.getLocalizedString(30201))
+  sys.exit(1)
 
 pl = xbmc.PlayList(1)
 ret = pl.load(XBMC_PLAYLIST)
+
+if ret == 0:
+  dialog = xbmcgui.Dialog()
+  dialog.ok(addon.getLocalizedString(30200), addon.getLocalizedString(30202))
+  sys.exit(1)
 
 for i in range(pl.size()):
   print "[%d]: %s: %s" % (i, pl[i].getdescription(), pl[i].getfilename())
